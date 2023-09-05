876a61304c1d67375c3e3c754c2a0e07c7751289
Carter Lee Rhea <carterrhea93@gmail.com>
2023-09-05 13:23:20 -0400

    Updated code to have a better deep image

--- interactiveBPT/interactiveBPT.py
+++ interactiveBPT/interactiveBPT.py
@@ -13,6 +13,10 @@ import numpy.ma as ma
 import matplotlib.pyplot as mpl
 import matplotlib.patches as patches
 import cmasher as cmr
+import base64
+from io import BytesIO
+import warnings
+warnings.filterwarnings("ignore")
 
 colors = cmr.take_cmap_colors('hsv', 10, return_fmt='hex')
 # Create some example data
@@ -25,7 +29,10 @@ def clean_flux(data_):
     #flux[flux<-22] = -22
     #flux[flux>-12] = -22
     return flux
-NGC_SN3_ref = fits.open('./NGC1275_lowres_deep.fits')[0]
+#NGC_SN3_ref = fits.open('./NGC1275_lowres_deep.fits')[0]
+#NGC_SN3_vel = fits.open('./NGC1275_lowres_3_sinc_Halpha_velocity.fits')[0]
+#NGC_SN3_broad = fits.open('./NGC1275_lowres_3_sinc_Halpha_broadening.fits')[0]
+
 # Read in data
 data_Halpha = clean_flux(fits.open('./NGC1275_lowres_3_sinc_Halpha_Amplitude.fits')[0].data)
 data_Halpha_flux = clean_flux(fits.open('./NGC1275_lowres_3_sinc_Halpha_Flux.fits')[0].data)
@@ -37,6 +44,7 @@ NII_Halpha = data_NII/data_Halpha
 Eq_width = data_Halpha_flux/data_SN3_continuum
 Eq_width_NII = data_NII/data_SN3_continuum
 
+image_paths = ['./NGC1275_lowres_deep.fits', './NGC1275_lowres_3_sinc_Halpha_velocity.fits', './NGC1275_lowres_3_sinc_Halpha_broadening.fits']
 
 front_NIIHalpha = profile_line(np.nan_to_num(NII_Halpha), (48,101), (82,115), linewidth=2, reduce_func=np.sum, mode='reflect')
 front_eqwHa = profile_line(np.nan_to_num(Eq_width), (48,101), (82,115), linewidth=2, reduce_func=np.sum, mode='reflect')
@@ -68,21 +76,7 @@ app = dash.Dash(__name__)
 
 image_data = fits.open('NGC1275_lowres_deep.fits')[0].data[200:1000, 1000:1500]
 image_data = np.arcsinh(image_data)
-'''fig, ax = plt.subplots(figsize=(8, 8))
-ax.imshow(image_data, cmap='viridis', origin='lower', vmin=np.nanpercentile(image_data, 5), vmax=np.nanpercentile(image_data, 99.5))  # Display the FITS image data as grayscale
-ax.axis('off')  # Turn off axis
 
-# Save the converted image to a BytesIO object
-from io import BytesIO
-buf = BytesIO()
-plt.savefig(buf, format='png', bbox_inches='tight', pad_inches=0)
-plt.close()
-
-# Get the base64-encoded image data
-import base64
-buf.seek(0)
-image_data_base64 = base64.b64encode(buf.read()).decode()
-'''
 app.layout = html.Div([
     html.Div([
         dcc.Graph(id='scatter-plot', config={'displayModeBar': False}),
@@ -96,16 +90,27 @@ app.layout = html.Div([
             ],
             value=[scatter['name'] for scatter in scatter_data],
             style={'margin-top': '10px'}
+        ),
+        dcc.Dropdown(
+            id='image-dropdown',
+            options=[
+                {'label': 'Deep Image', 'value': 0},
+                {'label': 'Velocity', 'value': 1},
+                {'label': 'Broadening', 'value': 2},
+                # Add more options for additional images
+            ],
+            value=0,
+            style={'margin-top': '10px'}
         )
     ], style={'width': '20%', 'display': 'inline-block', 'padding': '20px', 'vertical-align': 'middle'})
 ], style={'text-align': 'center'})
 # Define the callback function to update the scatter plot
 @app.callback(
-    [Output('scatter-plot', 'figure'), Output('image-display', 'src')],
-    Input('plot-selector', 'value')
+    [Output('scatter-plot', 'figure'), Output('image-display', 'src'), Output('image-display', 'style')],
+    [Input('plot-selector', 'value'), Input('image-dropdown', 'value'), Input('scatter-plot', 'hoverData')]
 )
 
-def update_scatter_plot(selected_plots):
+def update_scatter_plot(selected_plots, selected_image, hover_data):
     data = []
     rectangles = []
     for scatter, region in zip(scatter_data, region_data):
@@ -114,56 +119,38 @@ def update_scatter_plot(selected_plots):
                 x=scatter['x'],
                 y=scatter['y'],
                 mode='markers',
-                marker=dict(size=10, opacity=0.7, color=scatter['color']),
-                name=scatter['name']
+                marker=dict(size=10, opacity=0.5, color=scatter['color']),
+                name=scatter['name'], 
             )
             data.append(scatter_trace)
             # Add rectangles for the selected scatter plot
-            x_min = 3*min(region['x'])
-            x_max = 3*max(region['x'])
-            y_min = 3*min(region['y'])
-            y_max = 3*max(region['y'])
-            rect = patches.Rectangle(
-                (x_min, y_min), x_max - x_min, y_max - y_min,
-                linewidth=2, edgecolor=scatter['color'], facecolor='none'
-            )
-            rectangles.append(rect)
-    # Convert FITS image data to an image format (e.g., PNG) using matplotlib
-    fig, ax = plt.subplots(figsize=(8, 8))
-    ax.imshow(image_data, cmap='magma', origin='lower', vmin=np.nanpercentile(image_data, 5), vmax=np.nanpercentile(image_data, 99.5))  # Display the FITS image data as grayscale
-
-    for rect in rectangles:
-        ax.add_patch(rect)
-
-    ax.axis('off')  # Turn off axis
-
-    # Save the converted image to a BytesIO object
-    from io import BytesIO
-    buf = BytesIO()
-    plt.savefig(buf, format='png', bbox_inches='tight', pad_inches=0)
-    plt.close()
-
-    # Get the base64-encoded image data
-    import base64
-    buf.seek(0)
-    image_data_base64 = base64.b64encode(buf.read()).decode()
-
+            #x_min = 3*min(region['x'])
+            #x_max = 3*max(region['x'])
+            #y_min = 3*min(region['y'])
+            #_max = 3*max(region['y'])
+            #rect = patches.Rectangle(
+        #        (x_min, y_min), x_max - x_min, y_max - y_min,
+        #        linewidth=2, edgecolor=scatter['color'], facecolor='none'
+    #        )
+            #rectangles.append(rect)
     layout = go.Layout(
         title='Interactive Scatter Plot Toggle',
-        xaxis=dict(title='Log(<i>[NII]</i>/Hα)', tickformat='latex', showgrid=False),
-        yaxis=dict(title='W<sub>Hα</sub> [Å]', tickformat='latex', type='log', showgrid=False),
+        xaxis=dict(title='Log(<i>[NII]</i>/Hα)', tickformat='latex', showgrid=False, zeroline=False),
+        yaxis=dict(title='W<sub>Hα</sub> [Å]', tickformat='latex', type='log', showgrid=False, zeroline=False, dtick=1, showexponent= 'all', exponentformat= 'e'),
         xaxis_range=[-1, 1],  # Set your desired x-axis limits
         yaxis_range=[-1, 3],  # Set your desired y-axis limits
         width=1000,  # Set width to ensure square aspect ratio
         height=800,  # Set height to ensure square aspect ratio
-        margin=dict(l=40, r=40, b=40, t=60),  # Adjust plot margins
+        #margin=dict(l=40, r=40, b=40, t=60),  # Adjust plot margins
         shapes=[
+            # Vertical line extending across visible y-axis range
+            {'type': 'line', 'x0': -0.4, 'x1': -0.4, 'y0': -.1, 'y1': 1000, 'line': {'color': 'black', 'width': 1}},
             # Horizontal line extending across visible y-axis range
             {'type': 'line', 'x0': -1, 'x1': 1, 'y0': 6, 'y1': 6, 'line': {'color': 'black', 'width': 1}},
             # Dashed horizontal line extending across visible y-axis range
-            {'type': 'line', 'x0': -1, 'x1': 1, 'y0': 0.5, 'y1': 0.5, 'line': {'color': 'black', 'width': 1, 'dash': 'dash'}},
+            {'type': 'line', 'x0': -1, 'x1': 1, 'y0': 0.05, 'y1': 0.05, 'line': {'color': 'black', 'width': 1, 'dash': 'dash'}},
             # Sloped dashed line
-            {'type': 'line', 'x0': -1, 'x1': 0.75, 'y0': np.exp(-2.35 * -1 + np.log(0.5)), 'y1': np.exp(-2.35 * 0.75 + np.log(0.5)), 'line': {'color': 'black', 'width': 1, 'dash': 'dash'}}
+            #{'type': 'line', 'x0': -1, 'x1': 0.75, 'y0': np.exp(-2.35 * -1 + np.log(0.05)), 'y1': np.exp(-2.35 * 0.75 + np.log(0.05)), 'line': {'color': 'black', 'width': 1, 'dash': 'dash'}}
         ],
         annotations=[
             {'x': -0.8, 'y': 2, 'xref': 'x', 'yref': 'y', 'text': '<b>SF</b>', 'showarrow': False, 'font': {'size': 24}},
@@ -172,8 +159,54 @@ def update_scatter_plot(selected_plots):
             #{'x': -0.33, 'y': -0.3, 'xref': 'x', 'yref': 'y', 'text': 'Passive galaxies', 'showarrow': False, 'font': {'size': 15}}
         ]
     )
+    # Convert FITS image data to an image format (e.g., PNG) using matplotlib
+    fig, ax = plt.subplots(figsize=(8, 8))
+    # Read the selected image data using astropy
+    selected_image_path = image_paths[selected_image]
 
-    return {'data': data, 'layout': layout}, 'data:image/png;base64,{}'.format(image_data_base64)
+    if selected_image == 0:
+        selected_image_data = fits.open(selected_image_path)[0].data[200:1000, 1000:1500]
+        selected_image_data = np.arcsinh(selected_image_data)
+    else:
+        selected_image_data = fits.open(selected_image_path)[0].data
+        selected_image_data = selected_image_data
+
+    #ax.imshow(selected_image_data, cmap='magma', origin='lower', vmin=np.nanpercentile(selected_image_data, 5), vmax=np.nanpercentile(selected_image_data, 99.5))
+    ax.imshow(selected_image_data, cmap='magma', origin='lower', vmin=np.nanpercentile(image_data, 5), vmax=np.nanpercentile(image_data, 99.5))  # Display the FITS image data as grayscale
+
+    for rect in rectangles:
+        ax.add_patch(rect)
+
+    ax.axis('off')  # Turn off axis
+    # Highlight the hovered pixel if available
+    hover_style = {}
+    #print(hover_data['points'][0]['x'])
+    if hover_data is not None:
+        x_hover = hover_data['points'][0]['x']
+        y_hover = hover_data['points'][0]['y']
+        hover_style = {
+            'position': 'absolute',
+            'left': f'{x_hover * 100}%',  # Convert normalized coordinates to percentage
+            'top': f'{(1 - y_hover) * 100}%',  # Convert normalized coordinates to percentage
+            'width': '10px',
+            'height': '10px',
+            'border': '2px solid red',
+            'box-sizing': 'border-box',
+        }
+
+    # Save the converted image to a BytesIO object
+    buf = BytesIO()
+    plt.savefig(buf, format='png', bbox_inches='tight', pad_inches=0)
+    plt.close()
+
+    # Get the base64-encoded image data
+    buf.seek(0)
+    image_data_base64 = base64.b64encode(buf.read()).decode()
+
+
+
+
+    return {'data': data, 'layout': layout}, 'data:image/png;base64,{}'.format(image_data_base64), hover_style
 
 
 
