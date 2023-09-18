86372e04859a9978edadad67d6976b9550afedf6
Carter Lee Rhea <carterrhea93@gmail.com>
2023-09-18 19:46:20 -0400

    Updated to use masked data

--- interactiveBPT.py
+++ interactiveBPT.py
@@ -8,9 +8,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 from astropy.io import fits
 from skimage.measure import profile_line
-import time
-import numpy.ma as ma
-import matplotlib.pyplot as mpl
 import matplotlib.patches as patches
 import cmasher as cmr
 import base64
@@ -18,27 +15,18 @@ from io import BytesIO
 import warnings
 warnings.filterwarnings("ignore")
 
-colors = cmr.take_cmap_colors('hsv', 10, return_fmt='hex')
+colors = cmr.take_cmap_colors('hsv', 8, return_fmt='hex')
 # Create some example data
 def clean_flux(data_):
     flux = data_
-    #flux[np.isnan(flux)] = 1e-22
-    #flux[flux<1e-26] = np.nan
-    #flux[flux>1e-10] = np.nan
-    #flux = np.log10(flux)
-    #flux[flux<-22] = -22
-    #flux[flux>-12] = -22
     return flux
-#NGC_SN3_ref = fits.open('./NGC1275_lowres_deep.fits')[0]
-#NGC_SN3_vel = fits.open('./NGC1275_lowres_3_sinc_Halpha_velocity.fits')[0]
-#NGC_SN3_broad = fits.open('./NGC1275_lowres_3_sinc_Halpha_broadening.fits')[0]
 
 # Read in data
-data_Halpha = clean_flux(fits.open('./NGC1275_lowres_3_sinc_Halpha_Amplitude.fits')[0].data)
-data_Halpha_flux = clean_flux(fits.open('./NGC1275_lowres_3_sinc_Halpha_Flux.fits')[0].data)
+data_Halpha = clean_flux(fits.open('./NGC1275_3_Halpha_mask.fits')[0].data)
+data_Halpha_flux = clean_flux(fits.open('./NGC1275_3_Halpha_mask.fits')[0].data)
 
-data_NII = clean_flux(fits.open('./NGC1275_lowres_3_sinc_NII6583_Amplitude.fits')[0].data)
-data_SN3_continuum = fits.open('./NGC1275_lowres_3_sinc_continuum.fits')[0].data
+data_NII = clean_flux(fits.open('./NGC1275_3_NII6583_mask.fits')[0].data)
+data_SN3_continuum = fits.open('./NGC1275_3_sinc_continuum.fits')[0].data
 
 NII_Halpha = data_NII/data_Halpha
 Eq_width = data_Halpha_flux/data_SN3_continuum
@@ -52,11 +40,11 @@ front_eqwHa = profile_line(np.nan_to_num(Eq_width), (48,101), (82,115), linewidt
 scatter_data = [
     {'name': 'Central Region', 'color': colors[0], 'x': np.log10(NII_Halpha[45:115, 75:100].flatten()), 'y': Eq_width[45:115,75:100].flatten()},
     {'name': 'Star Forming Region', 'color': colors[1], 'x': np.log10(NII_Halpha[40:60,20:55].flatten()), 'y': Eq_width[40:60, 20:55].flatten()},
-    {'name': 'Horseshoe Knot', 'color': colors[2], 'x': np.log10(NII_Halpha[115:130,135:145].flatten()), 'y': Eq_width[115:130, 135:145].flatten()},
+    {'name': 'Horseshoe Knot', 'color': colors[2], 'x': np.log10(NII_Halpha[120:135,135:145].flatten()), 'y': Eq_width[115:130, 135:145].flatten()},
     {'name': 'Small Nothern Bar', 'color': colors[3], 'x': np.log10(NII_Halpha[80:90,140:175].flatten()), 'y': Eq_width[80:90,140:175].flatten()},
     {'name': 'Horseshoe Bottom', 'color': colors[4], 'x': np.log10(NII_Halpha[128:144,124:139].flatten()), 'y': Eq_width[128:144,124:139].flatten()},
-    {'name': 'Southern Claw', 'color': colors[5], 'x': np.log10(NII_Halpha[105:130, 15:35].flatten()), 'y': Eq_width[105:130, 15:35].flatten()},
-    {'name': 'Large Northern Bar', 'color': colors[6], 'x': np.log10(NII_Halpha[95:110, 140:240].flatten()), 'y': Eq_width[95:110, 140:240].flatten()},
+    {'name': 'Southern Claw', 'color': colors[5], 'x': np.log10(NII_Halpha[105:140, 15:45].flatten()), 'y': Eq_width[105:130, 15:35].flatten()},
+    {'name': 'Large Northern Bar', 'color': colors[6], 'x': np.log10(NII_Halpha[95:115, 140:240].flatten()), 'y': Eq_width[95:110, 140:240].flatten()},
     {'name': 'Shock Front', 'color': colors[7], 'x': np.log10(front_NIIHalpha), 'y': front_eqwHa}
 ]
 binning = 3
@@ -74,7 +62,7 @@ region_data = [
 # Initialize the Dash app
 app = dash.Dash(__name__)
 
-image_data = fits.open('NGC1275_lowres_deep.fits')[0].data[200:1000, 1000:1500]
+image_data = fits.open('NGC1275_lowres_deep.fits')[0].data[200:1000, 1000:1600]
 image_data = np.arcsinh(image_data)
 
 app.layout = html.Div([
@@ -114,7 +102,6 @@ app.layout = html.Div([
 
 def update_scatter_plot(selected_plots, selected_image, hover_data):
 #def update_scatter_plot(selected_plots, hover_data):
-
     data = []
     rectangles = []
     for scatter, region in zip(scatter_data, region_data):
@@ -138,11 +125,12 @@ def update_scatter_plot(selected_plots, selected_image, hover_data):
            )
             rectangles.append(rect)
     layout = go.Layout(
-        title='Interactive Scatter Plot Toggle',
+        #title='Equivalent Width BPT Map',
+        title=dict(text="Equivalent Width BPT Map", font=dict(size=30), automargin=True, yref='paper'),
         xaxis=dict(title='Log(<i>[NII]</i>/Hα)', tickformat='latex', showgrid=False, zeroline=False),
         yaxis=dict(title='W<sub>Hα</sub> [Å]', tickformat='latex', type='log', showgrid=False, zeroline=False, dtick=1, showexponent= 'all', exponentformat= 'e'),
         xaxis_range=[-1, 1],  # Set your desired x-axis limits
-        yaxis_range=[-1, 3],  # Set your desired y-axis limits
+        yaxis_range=[-1, 4],  # Set your desired y-axis limits
         width=1000,  # Set width to ensure square aspect ratio
         height=800,  # Set height to ensure square aspect ratio
         #margin=dict(l=40, r=40, b=40, t=60),  # Adjust plot margins
@@ -160,7 +148,6 @@ def update_scatter_plot(selected_plots, selected_image, hover_data):
             {'x': -0.8, 'y': 2, 'xref': 'x', 'yref': 'y', 'text': '<b>SF</b>', 'showarrow': False, 'font': {'size': 24}},
             {'x': 0.3, 'y': 2, 'xref': 'x', 'yref': 'y', 'text': '<b>Seyferts</b>', 'showarrow': False, 'font': {'size': 24}},
             {'x': 0.3, 'y': 0.25, 'xref': 'x', 'yref': 'y', 'text': '<b>LINERs</b>', 'showarrow': False, 'font': {'size': 24}},
-            #{'x': -0.33, 'y': -0.3, 'xref': 'x', 'yref': 'y', 'text': 'Passive galaxies', 'showarrow': False, 'font': {'size': 15}}
         ]
     )
     # Convert FITS image data to an image format (e.g., PNG) using matplotlib
@@ -169,7 +156,7 @@ def update_scatter_plot(selected_plots, selected_image, hover_data):
     # selected_image_path = image_paths[selected_image]
     selected_image_path = 'NGC1275_lowres_deep.fits'
     
-    selected_image_data = fits.open(selected_image_path)[0].data[200:1000, 1000:1500]
+    selected_image_data = fits.open(selected_image_path)[0].data[200:1000, 1000:1600]
     selected_image_data = np.arcsinh(selected_image_data)
     #if selected_image == 0:
     #    selected_image_data = fits.open(selected_image_path)[0].data[200:1000, 1000:1500]
